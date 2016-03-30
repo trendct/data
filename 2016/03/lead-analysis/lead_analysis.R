3f6c1212619204ba94cde122c9b8481aae345ce3
andrewbtran <abtran@gmail.com>
2016-03-30 09:19:37 -0400

    cleaning up

--- 2016/03/lead-analysis/lead_analysis.R
+++ 2016/03/lead-analysis/lead_analysis.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(ctnamecleaner)
 library(trendct)
-
+library(ggmap)
 ct <- read.csv("lead_ale_samples_ct.csv", stringsAsFactors=FALSE)
 
 actions <- read.csv("actions_count.csv", stringsAsFactors=FALSE)
@@ -23,6 +23,16 @@ ct_all_current <- ctnamecleaner(town, ct_all_current)
 out_of_state <- ct_all_current[is.na(ct_all_current$real.town.name),]
 ct_all_current <- ct_all_current[!is.na(ct_all_current$real.town.name),]
 
+ct_all_current$name_address <- paste(ct_all_current$name, ct_all_current$address, sep=", ")
+
+
+# This function geocodes a location (find latitude and longitude) using the Google Maps API
+geo <- geocode(location = ct_all_current$name_address, output="latlon", source="google")
+
+# add those coordinates to our dataset
+ct_all_current$lon <- geo$lon
+ct_all_current$lat <- geo$lat
+
 ct_all_current_df <- ct_all_current %>%
   group_by(real.town.name) %>%
   summarise(Count=n())
@@ -42,3 +52,4 @@ ct_all_current_u_df <- data.frame(ct_all_current_u_df)
 trendmap(ct_all_current_u_df, headline="Test map for R generator2", subhead="This is a subhead",
          src="something", byline="TrendCT.org", url_append="date", shape="towns", color="yellow-red")
 
+ct_all_current_unique$name_address <- paste(ct_all_current_unique$name, ct_all_current_unique$address, sep=", ")
--- readme.md
+++ readme.md
@@ -3,10 +3,5 @@
 | Story | Repo  | Data | Date |
 |---|---|---|---|
 | Where lead is in the water | [lead-analysis](https://github.com/trendct/data/tree/master/2016/03/lead-analysis)  |  [data](https://github.com/trendct/data/tree/master/2016/03/lead-analysis/data) |  3/31/16 |
-|   |   |   |   |
-|   |   |   |   |
-=======
-<a href="http://trendct.org">TrendCT.org</a>
->>>>>>> e88339d00cefe8f91919bb5895e355df98bd1955
 
 <a href="http://trendct.org">TrendCT.org</a>
